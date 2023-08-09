5b4e6e6665d03e60aa07d415bf7274f4377fa957
Artyom Pizin <artempetr2016@gmail.com>
2023-08-09 14:16:19 +0300

    feat: add static buildInputView method to the RegistrationView

--- src/view/RegistrationView/RegistrationView.ts
+++ src/view/RegistrationView/RegistrationView.ts
@@ -1,7 +1,23 @@
+enum InputTypes {
+  EMAIL = 'email',
+}
+
 export default class RegistrationView {
   public static buildRegistrationView(): HTMLFormElement {
     const form = document.createElement('form');
 
     return form;
+  }
+
+  private static buildInputView(type: InputTypes, pattern?: RegExp): HTMLInputElement {
+    const input = document.createElement('input');
+    input.type = type;
+    input.required = true;
+
+    if (pattern) {
+      input.pattern = `${pattern}`;
+    }
+
+    return input;
   }
 }
