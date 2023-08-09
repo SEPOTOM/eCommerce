042577248c6d52a39714d0473dee2406d87c361b
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-08-09 10:08:31 +0200

    feat: add autofixes

--- src/view/LoginView/LoginView.ts
+++ src/view/LoginView/LoginView.ts
@@ -1,51 +1,52 @@
 export default class LoginView {
-
   public static EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 
   public static PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s)(.{8,})$/;
 
   public static loginViewStyles = ['bg-black', 'm-0', 'opacity-75', 'absolute', 'inset-x-0', 'inset-y-0'];
 
-  public static loginWindowStyles = ['bg-white', 'w-2/4', 'h-2/4', 'text-center', 'm-auto', 'absolute', 'top-1/4', 'bottom-1/4', 'left-1/4', 'right-1/4', 'max-w-500'];
-
-  public static loginLabelAttributes = [
-    ['for', 'login']
+  public static loginWindowStyles = [
+    'bg-white',
+    'w-2/4',
+    'h-2/4',
+    'text-center',
+    'm-auto',
+    'absolute',
+    'top-1/4',
+    'bottom-1/4',
+    'left-1/4',
+    'right-1/4',
+    'max-w-500',
   ];
+
+  public static loginLabelAttributes = [['for', 'login']];
 
   public static loginInputAttributes = [
     ['type', 'text'],
     ['id', 'login'],
     ['oninvalid', `this.setCustomValidity(' ')`],
-    ['required', 'true']
+    ['required', 'true'],
   ];
 
-  public static passwordLabelAttributes = [
-    ['for', 'password']
-  ];
+  public static passwordLabelAttributes = [['for', 'password']];
 
   public static passwordInputAttributes = [
     ['type', 'password'],
     ['id', 'password'],
-    ['pattern', '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s)(.{8,})$'],
+    ['pattern', '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*s)(.{8,})$'],
     ['oninvalid', `this.setCustomValidity(' ')`],
-    ['required', 'true']
+    ['required', 'true'],
   ];
 
-  public static okButtonAttributes = [
-    ['id', 'submit-login']
-  ];
+  public static okButtonAttributes = [['id', 'submit-login']];
 
-  public static cancelButtonAttributes = [
-    ['id', 'cancel-login']
-  ];
+  public static cancelButtonAttributes = [['id', 'cancel-login']];
 
-  public static loginErrorAttributes = [
-    ['id', 'loginError']
-  ];
+  public static loginErrorAttributes = [['id', 'loginError']];
 
   public static passwordErrorAttributes = [
     ['id', 'passwordError'],
-    ['class', 'hidden']
+    ['class', 'hidden'],
   ];
 
   public static showLoginView(): void {
@@ -63,13 +64,13 @@ export default class LoginView {
   }
 
   public static addStyles(htmlElement: HTMLElement, styles: string[]): void {
-    styles.forEach(element => {
+    styles.forEach((element) => {
       htmlElement.classList.add(element);
     });
   }
 
   public static addAttributes(htmlElement: HTMLElement, styles: string[][]) {
-    styles.forEach(element => {
+    styles.forEach((element) => {
       htmlElement.setAttribute(element[0], element[1]);
     });
   }
@@ -142,38 +143,39 @@ export default class LoginView {
 
   public static addValidation() {
     const loginInput = document.getElementById('login');
-      const loginError = document.getElementById('loginError');
+    const loginError = document.getElementById('loginError');
 
-      const passwordInput = document.getElementById('password');
-      const passwordError = document.getElementById('passwordError');
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById('passwordError');
 
-      const submitButton: HTMLElement = document.getElementById('submit-login') as HTMLElement;
-      const passwordErrorText = 'Password does not meet a complexity criteria:<br>1. At least 8 characters long<br>2. At least one uppercase letter (A-Z)<br>3. At least one lowercase letter (a-z)<br>4. At least one digit (0-9)<br>5. At least one special character (!@#$%^&*)<br>6. Of no leading or trailing whitespace<br>'
-      const loginErrorText = 'Login does not meet requirements:<br>Email address must be properly formatted (e.g., user@example.com)<br>Email address must not contain leading or trailing whitespace<br>Email address must contain a domain name (e.g., example.com)<br>Email address must contain an &commat; symbol separating local part and domain name<br>';
+    const submitButton: HTMLElement = document.getElementById('submit-login') as HTMLElement;
+    const passwordErrorText =
+      'Password does not meet a complexity criteria:<br>1. At least 8 characters long<br>2. At least one uppercase letter (A-Z)<br>3. At least one lowercase letter (a-z)<br>4. At least one digit (0-9)<br>5. At least one special character (!@#$%^&*)<br>6. Of no leading or trailing whitespace<br>';
+    const loginErrorText =
+      'Login does not meet requirements:<br>Email address must be properly formatted (e.g., user@example.com)<br>Email address must not contain leading or trailing whitespace<br>Email address must contain a domain name (e.g., example.com)<br>Email address must contain an &commat; symbol separating local part and domain name<br>';
 
-      submitButton.addEventListener('click', () => {
+    submitButton.addEventListener('click', () => {
+      if (passwordInput.value.match(LoginView.PASSWORD_REGEX)) {
+        passwordError.style.display = 'none';
+      } else {
+        passwordError.style.display = 'block';
+        passwordError.innerHTML = passwordErrorText;
+      }
 
-        if (passwordInput.value.match(LoginView.PASSWORD_REGEX)) {
-          passwordError.style.display = 'none';
-        } else {
-          passwordError.style.display = 'block';
-          passwordError.innerHTML = passwordErrorText;
-        }
-
-        if (loginInput.value.match(LoginView.EMAIL_REGEX) && loginInput.value === loginInput.value.trim()) {
-          loginError.style.display = 'none';
-        } else {
-          loginError.style.display = 'block';
-          loginError.innerHTML = loginErrorText;
-        }
-      });
-
-      passwordInput.addEventListener('input', () => {
-          passwordError.style.display = 'none';
-      });
-
-      loginInput.addEventListener('input', () => {
+      if (loginInput.value.match(LoginView.EMAIL_REGEX) && loginInput.value === loginInput.value.trim()) {
         loginError.style.display = 'none';
-      });
+      } else {
+        loginError.style.display = 'block';
+        loginError.innerHTML = loginErrorText;
+      }
+    });
+
+    passwordInput.addEventListener('input', () => {
+      passwordError.style.display = 'none';
+    });
+
+    loginInput.addEventListener('input', () => {
+      loginError.style.display = 'none';
+    });
   }
 }
