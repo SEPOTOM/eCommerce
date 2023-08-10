eea26d081e224d7e303445ea522a1f3419359ccf
Artyom Pizin <artempetr2016@gmail.com>
2023-08-10 13:01:22 +0300

    refactor: replace validation.ts file with static validateInput method in RegistrationView

--- src/view/RegistrationView/RegistrationView.ts
+++ src/view/RegistrationView/RegistrationView.ts
@@ -1,6 +1,5 @@
-import { EventCallback } from './types';
-
-import { validateEmail, validatePassword } from './validation';
+const EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
+const PASSWORD_REGEXP = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s)(.{8,})$/;
 
 enum LabelTexts {
   EMAIL = 'Email',
@@ -16,43 +15,68 @@ export default class RegistrationView {
   public static buildRegistrationView(): HTMLFormElement {
     const form = document.createElement('form');
 
-    const emailRow = RegistrationView.buildRowView(LabelTexts.EMAIL, InputTypes.EMAIL, validateEmail);
+    const emailRow = RegistrationView.buildRowView(LabelTexts.EMAIL, InputTypes.EMAIL, EMAIL_REGEXP, LabelTexts.EMAIL);
     form.append(emailRow);
 
-    const passwordRow = RegistrationView.buildRowView(LabelTexts.PASSWORD, InputTypes.PASSWORD, validatePassword);
+    const passwordRow = RegistrationView.buildRowView(
+      LabelTexts.PASSWORD,
+      InputTypes.PASSWORD,
+      PASSWORD_REGEXP,
+      LabelTexts.PASSWORD
+    );
     form.append(passwordRow);
 
     return form;
   }
 
-  private static buildRowView(labelText: LabelTexts, inputType: InputTypes, callback?: EventCallback): HTMLElement {
+  private static buildRowView(
+    labelText: LabelTexts,
+    inputType: InputTypes,
+    regExp: RegExp,
+    fieldName: string
+  ): HTMLElement {
     const row = document.createElement('div');
 
-    const label = RegistrationView.buildLabelView(labelText, inputType, callback);
+    const label = RegistrationView.buildLabelView(labelText, inputType, regExp, fieldName);
     row.append(label);
 
     return row;
   }
 
-  private static buildLabelView(text: LabelTexts, inputType: InputTypes, callback?: EventCallback): HTMLLabelElement {
+  private static buildLabelView(
+    text: LabelTexts,
+    inputType: InputTypes,
+    regExp: RegExp,
+    fieldName: string
+  ): HTMLLabelElement {
     const label = document.createElement('label');
     label.textContent = text;
 
-    const input = RegistrationView.buildInputView(inputType, callback);
+    const input = RegistrationView.buildInputView(inputType, regExp, fieldName);
     label.append(input);
 
     return label;
   }
 
-  private static buildInputView(type: InputTypes, callback?: EventCallback): HTMLInputElement {
+  private static buildInputView(type: InputTypes, regExp: RegExp, fieldName: string): HTMLInputElement {
     const input = document.createElement('input');
     input.type = type;
     input.required = true;
 
-    if (callback) {
-      input.addEventListener('change', callback);
-    }
+    input.addEventListener('change', (e: Event): void => {
+      if (e.currentTarget instanceof HTMLInputElement) {
+        RegistrationView.validateInput(input, regExp, fieldName);
+      }
+    });
 
     return input;
+  }
+
+  private static validateInput(input: HTMLInputElement, regExp: RegExp, fieldName: string): void {
+    if (regExp.test(input.value)) {
+      console.log(`${fieldName} is valid!`);
+    } else {
+      console.error(`${fieldName} is invalid!`);
+    }
   }
 }
--- src/view/RegistrationView/types.ts
+++
@@ -1,1 +0,0 @@
-export type EventCallback = (e: Event) => void;
