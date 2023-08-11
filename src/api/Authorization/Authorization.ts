f3e86c0c87b08159df39c8fa78c6e676eadbf5a7
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-08-11 11:38:11 +0200

    refactor: add types for responses

--- src/api/Authorization/Authorization.ts
+++ src/api/Authorization/Authorization.ts
@@ -26,7 +26,7 @@ class Authorization {
   public static async loginClient(): Promise<IClientLoginResponse> {
     const queryString: string = `${CTP_AUTH_URL}/oauth/token?grant_type=client_credentials&scope=${CTP_SCOPES}`;
 
-    const response = await fetch(queryString, {
+    const response: Response = await fetch(queryString, {
       method: 'POST',
       headers: {
         Authorization: `Basic ${btoa(`${CTP_CLIENT_ID}:${CTP_CLIENT_SECRET}`)}`,
@@ -42,7 +42,7 @@ class Authorization {
   public static async loginBasicAuth(login: string, password: string): Promise<ICustomerLoginResponse> {
     const queryString: string = `${CTP_AUTH_URL}/oauth/${CTP_PROJECT_KEY}/customers/token?grant_type=password&username=${login}&password=${password}&scope=${CTP_SCOPES}`;
 
-    const response = await fetch(queryString, {
+    const response: Response = await fetch(queryString, {
       method: 'POST',
       headers: {
         Authorization: `Basic ${btoa(`${CTP_CLIENT_ID}:${CTP_CLIENT_SECRET}`)}`,
