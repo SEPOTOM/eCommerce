5cf8f4c0bc2b06b276e69e4e4218a05c00876ba6
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-08-12 17:44:06 +0200

    refactor: remove custom styles from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
     content: ["./src/**/*.{html,js,ts}"],
     darkMode: false,
     theme: {
-      extend: {
-        maxWidth: {
-          '500': '500px',
-        },
-        minWidth: {
-          '320': '320px',
-        }
-      },
+      extend: {},
     },
     variants: {},
     plugins: [],
