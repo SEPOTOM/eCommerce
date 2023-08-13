f7b9b032d04674a414dc15813579b96c311ec470
Artyom Pizin <artempetr2016@gmail.com>
2023-08-13 13:55:44 +0300

    refactor: move part of the constants to a separate file in the RegistrationView

--- src/view/RegistrationView/RegistrationView.ts
+++ src/view/RegistrationView/RegistrationView.ts
@@ -3,22 +3,10 @@ import SimpleInputView from './InputView/SimpleInputView/SimpleInputView';
 import DateInputView from './InputView/DateInputView/DateInputView';
 import SelectView from './SelectView/SelectView';
 import { InputOptions } from './types';
+import { RegExps, Countries } from './data';
 import HTML from './RegistrationView.html';
 
-const EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
-const PASSWORD_REGEXP = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s)(.{8,})$/;
-const LETTERS_REGEXP = /^[^\W\d_]+$/;
-const DATE_REGEXP = /^[0-9]{1,2}\/[0-9]{2,}\/[0-9]{4,}$/;
-const STREET_REGEXP = /^.+$/;
-const US_POSTAL_CODE_REGEXP_STRING = '(^\\d{5}$)|(^\\d{5}-\\d{4}$)';
-const UK_POSTAL_CODE_REGEXP_STRING =
-  '(^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})$)';
-const POSTAL_CODES_REGEXP = new RegExp(`${US_POSTAL_CODE_REGEXP_STRING}|${UK_POSTAL_CODE_REGEXP_STRING}`);
 const BIRTH_DATE_INPUT_INDEX = 4;
-const Countries = {
-  US: 'The United States',
-  UK: 'The United Kingdom',
-};
 
 enum InputTypes {
   EMAIL = 'email',
@@ -33,14 +21,14 @@ enum ClassNames {
 }
 
 const inputOptions: InputOptions[] = [
-  { regExp: EMAIL_REGEXP, type: InputTypes.EMAIL },
-  { regExp: PASSWORD_REGEXP, type: InputTypes.PASSWORD },
-  { regExp: LETTERS_REGEXP },
-  { regExp: LETTERS_REGEXP },
-  { regExp: DATE_REGEXP },
-  { regExp: STREET_REGEXP },
-  { regExp: LETTERS_REGEXP },
-  { regExp: POSTAL_CODES_REGEXP },
+  { regExp: RegExps.EMAIL, type: InputTypes.EMAIL },
+  { regExp: RegExps.PASSWORD, type: InputTypes.PASSWORD },
+  { regExp: RegExps.LETTERS },
+  { regExp: RegExps.LETTERS },
+  { regExp: RegExps.DATE },
+  { regExp: RegExps.STREET },
+  { regExp: RegExps.LETTERS },
+  { regExp: RegExps.POSTAL_CODES },
 ];
 
 export default class RegistrationView {
