0641f132a4477c97a7742fdceb41389ed8c08599
Mary Z <masha.zhirova@yandex.ru>
2023-08-16 20:53:54 +0300

    refactor: add changes after team code review

--- src/view/Page404/Page404.test.ts
+++
@@ -1,7 +0,0 @@
-import Page404 from './Page404';
-
-describe('Page404 general test', (): void => {
-  it('Class "Page404" should be defined', (): void => {
-    expect(Page404).toBeDefined();
-  });
-});
