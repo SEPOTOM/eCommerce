3d8d8611b1885e1d5a8bb1cca61d05c643fafdd6
Mary Z <masha.zhirova@yandex.ru>
2023-08-20 17:16:28 +0300

    refactor: moved images into project

--- src/view/HomepageView/HomepageView.ts
+++ src/view/HomepageView/HomepageView.ts
@@ -2,6 +2,11 @@ import HeaderView from '../HeaderView/HeaderView';
 import FooterView from '../FooterView/FooterView';
 import HomepageViewHTML from './HomepageView.html';
 
+// Import images to Home page
+import imgMainBanner from './../../assets/main-banner.jpg';
+import imgInfoBanner1 from './../../assets/banner-info1.jpg';
+import imgInfoBanner2 from './../../assets/banner-info2.jpg';
+
 export default class HomepageView {
   public drawDefaultContent(): void {
     const main = document.createElement('main');
@@ -17,5 +22,14 @@ export default class HomepageView {
   public draw(): void {
     const main: HTMLElement = document.querySelector('main')!;
     main.innerHTML = HomepageViewHTML;
+    
+    // Insert images
+    const mainBanner: HTMLElement = document.querySelector('[data-element="main-banner"]')!;
+    const infoBanner1: HTMLElement = document.querySelector('[data-element="info-banner-1"]')!;
+    const infoBanner2: HTMLElement = document.querySelector('[data-element="info-banner-2"]')!;
+
+    mainBanner.setAttribute('style', `background-image: url('${imgMainBanner}')`);
+    infoBanner1.setAttribute('style', `background-image: url('${imgInfoBanner1}')`);
+    infoBanner2.setAttribute('style', `background-image: url('${imgInfoBanner2}')`);
   }
 }
