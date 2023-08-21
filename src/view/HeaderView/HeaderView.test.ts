2c3bd28b96626b77bdef3209cf70acb541f5a264
Mary Z <masha.zhirova@yandex.ru>
2023-08-21 18:37:28 +0300

    refactor: improve test

--- src/view/HeaderView/HeaderView.test.ts
+++ src/view/HeaderView/HeaderView.test.ts
@@ -1,7 +1,28 @@
 import HeaderView from './HeaderView';
+import HeaderViewAlpine from './Alpine/HeaderViewAlpine';
 
-describe('HeaderView general test', (): void => {
-  it('Class "HeaderView" should be defined', (): void => {
+describe('Main [Header] test', (): void => {
+  it('-- сlass "HeaderView" should be defined', (): void => {
     expect(HeaderView).toBeDefined();
+  });
+
+  it('-- method "draw" should be defined', () => {
+    expect(new HeaderView().draw).toBeDefined();
+  });
+
+  it('-- method "draw" should return nothing', () => {
+    expect(new HeaderView().draw()).toBeFalsy();
+  });
+
+  it('-- Alpine header object has property => showMobileMenu', () => {
+    expect(HeaderViewAlpine()).toHaveProperty('showMobileMenu');
+  });
+
+  it('-- Alpine header object has property => showAccountDropdown', () => {
+    expect(HeaderViewAlpine()).toHaveProperty('showAccountDropdown');
+  });
+
+  it('-- Alpine header object has property => menu', () => {
+    expect(HeaderViewAlpine()).toHaveProperty('menu');
   });
 });
