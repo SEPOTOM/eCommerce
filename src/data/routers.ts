5b8d45a48a8ac21f37943b69616ee2232678ca1d
Artyom Pizin <artempetr2016@gmail.com>
2023-08-27 13:01:52 +0300

    feat(routing): add the ProfileView component to the profile page

--- src/data/routers.ts
+++ src/data/routers.ts
@@ -3,20 +3,14 @@ import LoginView from '../view/LoginView/LoginView';
 import HomepageView from '../view/HomepageView/HomepageView';
 import Page404View from '../view/Page404View/Page404View';
 import RegistrationView from '../view/RegistrationView/RegistrationView';
+import ProfileView from '../view/ProfileView/ProfileView';
 
 const routers: { [key: string]: () => void } = {
   '404': new Page404View().draw,
   '/': new HomepageView().draw,
   '/login': () => LoginView.draw(),
   '/registration': () => RegistrationView.draw(),
-  '/profile': () => {
-    const main = document.querySelector('main');
-
-    if (main) {
-      main.innerHTML =
-        '<div class="flex justify-center items-center flex-grow basis-full text-2xl">profile page placeholder</div>';
-    }
-  },
+  '/profile': () => new ProfileView().draw(),
 };
 
 export default routers;
