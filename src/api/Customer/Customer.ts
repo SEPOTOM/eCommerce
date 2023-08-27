fdabe2539ba5c3260ab6098bd54b022db794f54c
Artyom Pizin <artempetr2016@gmail.com>
2023-08-27 12:53:45 +0300

    refactor(Customer): remove setId and getId methods

--- src/api/Customer/Customer.ts
+++ src/api/Customer/Customer.ts
@@ -3,24 +3,12 @@ import Tokens from '../../components/Tokens/Tokens';
 import { CTP_API_URL, CTP_PROJECT_KEY } from '../APIClients/JSNinjas-custom';
 import { CustomerResponse } from '../../types';
 
-enum StorageNames {
-  ID = 'customer-id',
-}
-
 enum ErrorMessages {
   NO_CUSTOMER = 'The customer with the given ID was not found',
   SERVER = 'Failed to connect to the server',
 }
 
 export default class Customer {
-  public static setId(id: string): void {
-    localStorage.setItem(StorageNames.ID, id);
-  }
-
-  public static getId(): string | null {
-    return localStorage.getItem(StorageNames.ID) || null;
-  }
-
   public static async getCurrentCustomer(): Promise<CustomerResponse | Error> {
     try {
       const endpoint = `${CTP_API_URL}/${CTP_PROJECT_KEY}/me`;
