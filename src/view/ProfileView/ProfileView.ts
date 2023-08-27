3f350ab5237c7939fb49473bcb461cf0e6d4a449
Artyom Pizin <artempetr2016@gmail.com>
2023-08-27 15:31:03 +0300

    feat(ProfileView): add "User Info" section

--- src/view/ProfileView/ProfileView.ts
+++ src/view/ProfileView/ProfileView.ts
@@ -1,8 +1,17 @@
 import Converter from '../../components/Converter/Converter';
+/* eslint-disable import/no-cycle */
+import Customer from '../../api/Customer/Customer';
 import HTML from './ProfileView.html';
+import ParagraphView from './ParagraphView/ParagraphView';
+import { CustomerDataResponse } from '../../types';
+import { DataAttrs, ParagraphLabels } from './data';
 
 export default class ProfileView {
   private view = Converter.htmlToElement<HTMLDivElement>(HTML) || document.createElement('div');
+
+  constructor() {
+    this.configureView();
+  }
 
   public draw(): void {
     const main = document.querySelector('main');
@@ -10,6 +19,28 @@ export default class ProfileView {
     if (main) {
       main.innerHTML = '';
       main.append(this.view);
+    }
+  }
+
+  private async configureView(): Promise<void> {
+    const customerData = await Customer.getCurrentCustomer();
+
+    if (!('message' in customerData)) {
+      this.configureUserInfo(customerData);
+    }
+  }
+
+  private configureUserInfo(customerData: CustomerDataResponse): void {
+    const section = this.view.querySelector(`[${DataAttrs.USER_INFO}]`);
+
+    section?.append(new ParagraphView().buildView(ParagraphLabels.FIRST_NAME, customerData.firstName));
+    section?.append(new ParagraphView().buildView(ParagraphLabels.LAST_NAME, customerData.lastName));
+
+    if (typeof customerData.dateOfBirth === 'string') {
+      const [year, month, day] = customerData.dateOfBirth.split('-');
+      const formattedDate = `${month}/${day}/${year}`;
+
+      section?.append(new ParagraphView().buildView(ParagraphLabels.BIRTH_DATE, formattedDate));
     }
   }
 }
---
+++ src/view/ProfileView/data.ts
@@ -0,0 +1,9 @@
+export enum DataAttrs {
+  USER_INFO = 'data-user-info',
+}
+
+export enum ParagraphLabels {
+  FIRST_NAME = 'First name:',
+  LAST_NAME = 'Last name:',
+  BIRTH_DATE = 'Date of birth:',
+}
