d6fede96cff13cc41745b1cee7a4dc5011d1f638
Artyom Pizin <artempetr2016@gmail.com>
2023-08-27 12:52:15 +0300

    revert: "feat(Registration): save user ID after successful registration"
    This reverts commit f1de591f717e80758578b5fd96cd43d15e3b0960.

--- src/api/Registration/Registration.ts
+++ src/api/Registration/Registration.ts
@@ -10,7 +10,6 @@ import { CustomerCredentials, ResponseInfo, RegErrorResponse, CustomerResponse }
 import Authorization from '../Authorization/Authorization';
 /* eslint-disable import/no-cycle */
 import Tokens from '../../components/Tokens/Tokens';
-import Customer from '../Customer/Customer';
 
 enum ErrorMessages {
   SERVER = 'Failed to connect to the server. Please check your network connection or try again later.',
@@ -55,7 +54,6 @@ export default class Registration {
         result.message = this.getMessage(data);
       } else {
         result.ok = true;
-        Customer.setId(data.customer.id);
       }
     } catch (err) {
       return result;
