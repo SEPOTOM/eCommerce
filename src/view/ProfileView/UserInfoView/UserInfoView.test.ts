50fd1ff604de524b16c76de523ae9b6b0a42abb7
Artyom Pizin <artempetr2016@gmail.com>
2023-08-31 17:47:47 +0300

    feat(ProfileView): add tests

--- src/view/ProfileView/UserInfoView/UserInfoView.test.ts
+++ src/view/ProfileView/UserInfoView/UserInfoView.test.ts
@@ -10,4 +10,20 @@ describe('UserInfoView class', () => {
   it('an instance must have a buildView method', () => {
     expect(instance.buildView).toBeDefined();
   });
+
+  it('an instance must have a enterEditMode method', () => {
+    expect(instance.enterEditMode).toBeDefined();
+  });
+
+  it('an instance must have a exitEditMode method', () => {
+    expect(instance.exitEditMode).toBeDefined();
+  });
+
+  it('an instance must have a collectCredentials method', () => {
+    expect(instance.collectCredentials).toBeDefined();
+  });
+
+  it('an instance must have a updateInfo method', () => {
+    expect(instance.updateInfo).toBeDefined();
+  });
 });
