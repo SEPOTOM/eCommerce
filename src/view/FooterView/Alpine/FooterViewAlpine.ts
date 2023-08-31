8a34d9441e0f959311be868ca807d9409bd04483
Mary Z <masha.zhirova@yandex.ru>
2023-08-31 19:22:23 +0300

    refactor: fix naming

--- src/view/FooterView/Alpine/FooterViewAlpine.ts
+++ src/view/FooterView/Alpine/FooterViewAlpine.ts
@@ -3,7 +3,7 @@ import imgFooterBackground from '../../../assets/footer-background.jpg';
 
 export default () => ({
   menu: null,
-  footerBaclground: `url(${imgFooterBackground})`,
+  footerBackground: `url(${imgFooterBackground})`,
 
   init(): void {
     Navigation.menu.then((data): void => {
