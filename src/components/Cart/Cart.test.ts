ec4ae0e4599ad687afa9baafc781052182908962
Artyom Pizin <artempetr2016@gmail.com>
2023-09-07 15:39:24 +0300

    feat(Cart): add tests

---
+++ src/components/Cart/Cart.test.ts
@@ -0,0 +1,17 @@
+import Cart from './Cart';
+
+describe('Cart class', () => {
+  const instance = new Cart();
+
+  it('must be defined', () => {
+    expect(Cart).toBeDefined();
+  });
+
+  it('an instance must have a getCart method', () => {
+    expect(instance.getCart).toBeDefined();
+  });
+
+  it('an instance must have a getProductsInfo method', () => {
+    expect(instance.getProductsInfo).toBeDefined();
+  });
+});
