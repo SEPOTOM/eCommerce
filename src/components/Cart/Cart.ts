d8356d9c47a001cb16e133638f972aeec367246b
Artyom Pizin <artempetr2016@gmail.com>
2023-09-08 20:28:33 +0300

    feat(Cart): add updateCurrentCart method

--- src/components/Cart/Cart.ts
+++ src/components/Cart/Cart.ts
@@ -1,14 +1,23 @@
 /* eslint-disable import/no-cycle */
 import CartAPI from '../../api/CartAPI/CartAPI';
 import Converter from '../Converter/Converter';
-import { CartInfo, ProductInfo } from '../../types';
+import { CartInfo, ProductInfo, CartResponse } from '../../types';
 
 export default class Cart {
   private cart: CartInfo | null = null;
 
   public async getCart(): Promise<Cart | Error> {
     const cartResponse = await CartAPI.get();
+    const cart = this.updateCurrentCart(cartResponse);
 
+    return cart;
+  }
+
+  public getProductsInfo(): ProductInfo[] {
+    return this.cart?.productsInfo || [];
+  }
+
+  private updateCurrentCart(cartResponse: CartResponse | Error): Cart | Error {
     if ('message' in cartResponse) {
       return cartResponse;
     }
@@ -16,9 +25,5 @@ export default class Cart {
     this.cart = Converter.cartResponseToInfo(cartResponse);
 
     return this;
-  }
-
-  public getProductsInfo(): ProductInfo[] {
-    return this.cart?.productsInfo || [];
   }
 }
