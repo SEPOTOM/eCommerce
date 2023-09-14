c39687add6370312dfcdda72198ffd68e4ba7c4e
Artyom Pizin <artempetr2016@gmail.com>
2023-09-14 15:52:10 +0300

    refactor(CartAPI): update error messages

--- src/api/CartAPI/CartAPI.ts
+++ src/api/CartAPI/CartAPI.ts
@@ -6,9 +6,14 @@ import { GlobalErrorMessages } from '../../data/errors';
 import { CartResponse, IError, CartsResponse, IAddLineItem, ICartTemplate } from '../../types';
 
 enum ErrorMessages {
-  SERVER = 'Failed to connect to the server. Please, check your connection or try again later.',
+  SERVER = 'Operation error. Please, try later.',
   TRY_LATER = 'Please, try again later.',
   LOG_IN = 'Please, login to get to a cart.',
+  WRONG_PROMO = 'Wrong promo code.',
+}
+
+enum ErrorCodes {
+  WRONG_PROMO = 'DiscountCodeNonApplicable',
 }
 
 export default class CartAPI {
@@ -101,42 +106,6 @@ export default class CartAPI {
     return this.sendRequest(endpoint, requestOptions);
   }
 
-  private static async sendRequest(endpoint: string, requestOptions: RequestInit): Promise<CartResponse | Error> {
-    const tokens = await Tokens.getCustomerTokens();
-
-    if (!tokens || !tokens.access_token) {
-      return new Error(ErrorMessages.LOG_IN);
-    }
-
-    const bearerToken = tokens.access_token;
-
-    try {
-      const response = await fetch(endpoint, {
-        ...requestOptions,
-        headers: {
-          ...requestOptions.headers,
-          Authorization: `Bearer ${bearerToken}`,
-        },
-      });
-      const data: CartsResponse | CartResponse | IError = await response.json();
-
-      if ('message' in data) {
-        return new Error(`${data.message} ${ErrorMessages.TRY_LATER}`);
-      }
-
-      if ('results' in data && data.results[0]) {
-        return data.results[0];
-      }
-      if ('results' in data) {
-        return new Error(GlobalErrorMessages.NO_CART);
-      }
-
-      return data;
-    } catch (err) {
-      return new Error(ErrorMessages.SERVER);
-    }
-  }
-
   public static async updateLineItem(cartID: string, payload: IAddLineItem): Promise<CartResponse | Error> {
     const endpoint = `${CTP_API_URL}/${CTP_PROJECT_KEY}/carts/${cartID}`;
     const requestOptions = {
@@ -182,5 +151,49 @@ export default class CartAPI {
     }
 
     return cartVersion;
+  }
+
+  private static async sendRequest(endpoint: string, requestOptions: RequestInit): Promise<CartResponse | Error> {
+    const tokens = await Tokens.getCustomerTokens();
+
+    if (!tokens || !tokens.access_token) {
+      return new Error(ErrorMessages.LOG_IN);
+    }
+
+    const bearerToken = tokens.access_token;
+
+    try {
+      const response = await fetch(endpoint, {
+        ...requestOptions,
+        headers: {
+          ...requestOptions.headers,
+          Authorization: `Bearer ${bearerToken}`,
+        },
+      });
+      const data: CartsResponse | CartResponse | IError = await response.json();
+
+      if ('message' in data) {
+        return this.defineError(data);
+      }
+
+      if ('results' in data && data.results[0]) {
+        return data.results[0];
+      }
+      if ('results' in data) {
+        return new Error(GlobalErrorMessages.NO_CART);
+      }
+
+      return data;
+    } catch (err) {
+      return new Error(ErrorMessages.SERVER);
+    }
+  }
+
+  private static defineError(error: IError): Error {
+    if (error.errors[0].code === ErrorCodes.WRONG_PROMO) {
+      return new Error(ErrorMessages.WRONG_PROMO);
+    }
+
+    return new Error(`${error.message} ${ErrorMessages.TRY_LATER}`);
   }
 }
